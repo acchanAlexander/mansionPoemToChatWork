a84e63d74f9061d440b4a48c79b31f71fb7ece6b
acchanAlexander <AtariYoshihiro@next-group.jp>
2017-02-04 10:22:02 +0100

    npmでchatworkモジュールを使うように修正

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 const fs = require('fs')
     , mansionPoem = require('./mansionPoem')
-    , chatwork = require('./chatwork/chatwork')
+    , chatwork = require('chatwork-api-v2')
     ;
 
 let chatworkParams = {
